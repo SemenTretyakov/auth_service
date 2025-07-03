124b0cacbb0bcadcdf83710b2c643c15ff7859e0
s.s.tretyakov <s.s.tretyakov@tmn3.etagi.com>
2025-07-03 21:07:52 +0500

    fix c

--- cmd/grpc_server/main.go
+++ cmd/grpc_server/main.go
@@ -89,7 +89,7 @@ func main() {
 	reflection.Register(s)
 	desc.RegisterUserV1Server(s, &server{})
 
-	log.Printf("server listening at PORT %v", lis.Addr())
+	log.Printf("server listening at %v", lis.Addr())
 
 	if err = s.Serve(lis); err != nil {
 		log.Fatalf("failed to Serve: %v", err)
